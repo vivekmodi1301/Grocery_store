5d1e08ee8615a7988e977111365cdde192c51b55
vivekmodi1301 <modivivek03@gmail.com>
2023-06-13 14:00:29 +0530

    ui

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,18 @@
 from flask import Flask , request , jsonify
+import products_dao
+
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+connection = get_sql_connection()
+@app.route("/get_products", methods = ['GET'])
+def get_products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
 
 if __name__ == "__main__":
     print("start python Flask server for Grocery store management system")
