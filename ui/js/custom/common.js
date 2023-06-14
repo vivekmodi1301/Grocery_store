5464dbf01b3c57e6a95457ce1cdaf5017d2d7de5
vivekmodi1301 <modivivek03@gmail.com>
2023-06-14 13:17:18 +0530

    ui changes

--- ui/js/custom/common.js
+++ ui/js/custom/common.js
@@ -1,5 +1,5 @@
 // Define your api here
-var productListApiUrl = 'http://127.0.0.1:5000/get_products';
+var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
 var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
 var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
 var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
